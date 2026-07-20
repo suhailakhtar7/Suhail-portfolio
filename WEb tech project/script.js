6056fd1e0bfb7d46574f80a0d52147b32ecbdf58
S <suhail.akhtar@rungta.org>
2026-07-20 22:06:31 +0530

    Update script.js

--- WEb tech project/script.js
+++ WEb tech project/script.js
@@ -1,11 +1,80 @@
-function openCert(imagePath) {
-    const modal = document.getElementById("modal");
-    const modalImg = document.getElementById("modalImg");
+// Typing Animation
 
-    modal.style.display = "flex";
-    modalImg.src = imagePath;
-}
+const text = [
+  "Software Developer",
+  "Java Programmer",
+  "AWS Community Member",
+  "Problem Solver"
+];
 
-function closeCert() {
-    document.getElementById("modal").style.display = "none";
-}
+let count = 0;
+let index = 0;
+let currentText = "";
+let letter = "";
+
+(function type() {
+
+  if (count === text.length) {
+    count = 0;
+  }
+
+  currentText = text[count];
+  letter = currentText.slice(0, ++index);
+
+  document.getElementById("typing").textContent = letter;
+
+  if (letter.length === currentText.length) {
+
+    setTimeout(() => {
+
+      index = 0;
+      count++;
+
+      setTimeout(type, 300);
+
+    }, 1500);
+
+  } else {
+
+    setTimeout(type, 100);
+
+  }
+
+})();
+
+
+// Navbar Background Change
+
+window.addEventListener("scroll", function () {
+
+  const header = document.querySelector("header");
+
+  if (window.scrollY > 50) {
+
+    header.style.background = "#08111f";
+
+  } else {
+
+    header.style.background = "rgba(15,23,42,.8)";
+
+  }
+
+});
+
+
+// Smooth Scroll
+
+document.querySelectorAll('a[href^="#"]').forEach(anchor => {
+
+  anchor.addEventListener("click", function (e) {
+
+    e.preventDefault();
+
+    document.querySelector(this.getAttribute("href"))
+      .scrollIntoView({
+        behavior: "smooth"
+      });
+
+  });
+
+});
